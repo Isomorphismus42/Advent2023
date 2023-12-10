d1f1b8cdad72da0895aa0ee55982ff503984c404
Isomorphismus42 <F.Kazemi@hotmail.de>
2023-12-10 20:51:37 +0100

    Day 10 - Part 2

--- Day10.py
+++ Day10.py
@@ -120,65 +120,179 @@ position to the point farthest from the starting position?
 
 ------------------- Part Two --------------------
 
+You quickly reach the farthest point of the loop, but the animal never emerges. Maybe its nest is within the area
+enclosed by the loop?
 
+To determine whether it's even worth taking the time to search for such a nest, you should calculate how many tiles
+are contained within the loop. For example:
+
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+
+The above loop encloses merely four tiles - the two pairs of . in the southwest and southeast (marked I below). The
+middle . tiles (marked O below) are not in the loop. Here is the same loop again with those regions marked:
+
+...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+
+In fact, there doesn't even need to be a full tile path to the outside for tiles to count as outside the loop -
+squeezing between pipes is also allowed! Here, I is still within the loop and O is still outside the loop:
+
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+
+In both of the above examples, 4 tiles are enclosed by the loop.
+
+Here's a larger example:
+
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+
+The above sketch has many random bits of ground, some of which are in the loop (I) and some of which are outside it (O):
+
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+
+In this larger example, 8 tiles are enclosed by the loop.
+
+Any tile that isn't part of the main loop can count as being enclosed by the loop. Here's another example with many
+bits of junk pipe lying around that aren't connected to the main loop at all:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+
+Here are just the tiles that are enclosed by the loop marked with I:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+
+In this last example, 10 tiles are enclosed by the loop.
+
+Figure out whether you have time to search for the nest by calculating the area within the loop. How many tiles are
+enclosed by the loop?
 
 """
 
-# import numpy as np
+import numpy as np
 # import re
 # from collections import OrderedDict
 
+field = open('inputs/input_day10', 'r').read().splitlines()
+
 
 def find_circle():
-    field = open('inputs/input_day10', 'r').read().splitlines()
-    for j, line in enumerate(field):
+    for i, line in enumerate(field):
         start = line.find("S")
         if start != -1:
-            start = (start, j)
+            start = (i, start)
             break
     circle = list()
     position = start
     while True:
         circle.append(position)
         i, j = position
-        pipe_type = field[j][i]
+        pipe_type = field[i][j]
         if pipe_type == "|":
-            if prev_position == (i, j - 1):
-                position = (i, j + 1)
+            if prev_position == (i - 1, j):
+                position = (i + 1, j)
             else:
-                position = (i, j - 1)
+                position = (i - 1, j)
         elif pipe_type == "-":
-            if prev_position == (i - 1,j):
-                position = (i + 1, j)
-            else:
-                position = (i - 1, j)
-        elif pipe_type == "L":
             if prev_position == (i, j - 1):
-                position = (i + 1, j)
-            else:
-                position = (i, j - 1)
-        elif pipe_type == "J":
-            if prev_position == (i, j - 1):
-                position = (i - 1, j)
-            else:
-                position = (i, j - 1)
-        elif pipe_type == "7":
-            if prev_position == (i, j + 1):
-                position = (i - 1, j)
-            else:
                 position = (i, j + 1)
+            else:
+                position = (i, j - 1)
+        elif pipe_type == "L":
+            if prev_position == (i - 1, j):
+                position = (i, j + 1)
+            else:
+                position = (i - 1, j)
+        elif pipe_type == "J":
+            if prev_position == (i - 1, j):
+                position = (i, j - 1)
+            else:
+                position = (i - 1, j)
+        elif pipe_type == "7":
+            if prev_position == (i, j - 1):
+                position = (i + 1, j)
+            else:
+                position = (i, j - 1)
         elif pipe_type == "F":
             if prev_position == (i, j + 1):
                 position = (i + 1, j)
             else:
                 position = (i, j + 1)
         elif pipe_type == "S":
-            if field[j][i + 1] in {"-", "7", "J"}:
-                position = (i + 1, j)
-            elif field[j][i - 1] in {"-", "F", "L"}:
-                position = (i - 1, j)
-            else:
+            if field[i][j + 1] in {"-", "7", "J"}:
                 position = (i, j + 1)
+                if field[i][j - 1] in {"-", "F", "L"}:
+                    field[i] = field[i][:j] + "-" + field[i][j + 1:]
+                elif field[i - 1][j] in {"|", "F", "7"}:
+                    field[i] = field[i][:j] + "L" + field[i][j + 1:]
+                elif field[i + 1][j] in {"|", "J", "L"}:
+                    field[i] = field[i][:j] + "F" + field[i][j + 1:]
+            elif field[i][j - 1] in {"-", "F", "L"}:
+                position = (i, j - 1)
+                if field[i - 1][j] in {"|", "F", "7"}:
+                    field[i] = field[i][:j] + "J" + field[i][j + 1:]
+                elif field[i + 1][j] in {"|", "J", "L"}:
+                    field[i] = field[i][:j] + "7" + field[i][j + 1:]
+            else:
+                position = (i + 1, j)
+                field[i] = field[i][:j] + "|" + field[i][j + 1:]
         if position == start:
             break
         prev_position = (i, j)
@@ -190,12 +304,40 @@ def part1():
 
 
 def part2():
-    # Schritt 1: Orientierung herausfinden - welche Seite des Kreises zeigt nach innen?
-    # Schritt 2: Kreis ablaufen - wenn angrenzendes "inneres Feld" nicht Teil von Kreis oder bereits gefunden, dann +1
-    # und Breitensuche über angrenzende Felder, die nicht Teil Kreis und nicht gefunden.
+    circle = set(find_circle())
+    m = len(field)
+    n = len(field[0])
 
-    return 0
+    parity_matrix = np.ones((m, n))
+    for i in range(m):
+        parity = False
+        last = ''
+        for j in range(n):
+            if (i, j) in circle:
+                edge = field[i][j]
+                if edge == '|' or (edge == '7' and last == 'L') or (edge == 'J' and last == 'F'):
+                    parity = not parity
+                    last = ''
+                elif edge in {'7', 'L', 'J', 'F'}:
+                    last = edge
+                parity_matrix[i, j] = 0
+            elif not parity:
+                parity_matrix[i, j] = 0
+    for j in range(n):
+        parity = False
+        last = ''
+        for i in range(m):
+            if (i, j) in circle:
+                edge = field[i][j]
+                if edge == '-' or (edge == 'L' and last == '7') or (edge == 'J' and last == 'F'):
+                    parity = not parity
+                    last = ''
+                elif edge in {'7', 'L', 'J', 'F'}:
+                    last = edge
+            elif not parity:
+                parity_matrix[i, j] = 0
+    return int(np.sum(parity_matrix))
 
 
 if __name__ == "__main__":
-    print(part1())
+    print(part2())
